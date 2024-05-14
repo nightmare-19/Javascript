feaf8e24d39e72186c2e72806493969d28a9be3c
nightmare-19 <54665607+nightmare-19@users.noreply.github.com>
2024-05-14 07:02:24 +0000

    Conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,10 @@
+/*let score = "uzhair"
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(score)*/
+
+let score = 33
+let stringNumber = String(score)
+console.log(typeof stringNumber)
+console.log(stringNumber)
