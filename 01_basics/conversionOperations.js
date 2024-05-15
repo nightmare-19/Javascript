70b5f2ecda350dd434ad20a159092fa3fc3b59f2
nightmare-19 <54665607+nightmare-19@users.noreply.github.com>
2024-05-15 05:53:03 +0000

    strings and methods

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -4,7 +4,12 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 console.log(score)*/
 
-let score = 33
-let stringNumber = String(score)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// let score = 33
+// let stringNumber = String(score)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+console.log("2">1)
+console.log(null >= 0)
+console.log(null == 0)
+console.log(null === 0)
---
+++ 01_basics/test.js
@@ -0,0 +1,4 @@
+console.log("Hi")
+let a;
+a = 12;
+console.log(a)
