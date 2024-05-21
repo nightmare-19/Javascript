296e87443569ca85b942e6d0844254522059ae14
nightmare-19 <54665607+nightmare-19@users.noreply.github.com>
2024-05-21 09:29:11 +0000

    for loop, break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,38 @@
+//for
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value is : ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        //console.log(i + '*' + j + '=' + i*j);
+        }    
+}
+
+let myArray = ["flash" , "batman" , "superman"]
+//console.log(myArray.length);
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    //console.log(element);
+}
+
+for (let i = 1; i < 20; i++) {
+    if(i==5)
+        {
+         //console.log(`5 detected`);
+         break;   
+        }
+    //console.log(`Value of i is ${i}`);
+}
+for (let i = 1; i < 20; i++) {
+    if(i==5)
+        {
+         console.log(`5 detected`);
+         continue;   
+        }
+    console.log(`Value of i is ${i}`);
+}
+
