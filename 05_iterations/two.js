1dd19a840d6a2f3a7d93f8c33d0f0aeedc807476
nightmare-19 <54665607+nightmare-19@users.noreply.github.com>
2024-05-21 09:56:43 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+//while
+let i=0;
+while(i<=10)
+    {
+       //console.log(`Value of i is ${i}`); 
+       i = i+2;
+    }
+
+let myArray = ['batman', 'flash', 'superman']
+let arr=0;
+while(arr<myArray.length)
+    {
+        //console.log(myArray[arr]);
+        arr++;
+    }
+
+    let score=11
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1;
+} while (score <=10);
