0293fc9ebc51083bc34a3f08f87efe4702dcb66d
nightmare-19 <54665607+nightmare-19@users.noreply.github.com>
2024-05-21 11:00:26 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+const myObj = {
+    js : "javascript",
+    cpp : "C++",
+    rv : "Ruby",
+    swift : "Swift by apple"
+}
+
+for (const key in myObj) {
+    //console.log(`${key} shortcut is for ${myObj[key]}`);
+}
+
+const myArr = ['js','ruby','python','java','cpp']
+for (const key in myArr) {
+   console.log(myArr[key]);
+}
+
